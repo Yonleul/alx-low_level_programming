0b0b493ab652f4f8f5c483f571a9c34687e9c9d1
Yonleul <yonasleulbej@gmail.com>
2023-06-15 11:44:35 -0700

    Task 4 level C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
 /**
- * main-Printing
- *
- * Return: Always 0 (Successful)
+ * main - Entry point
+ * Description: prints "\"Programming is like building a multilingual puzzle"
+ * Return: 0
  */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-}	
+}
